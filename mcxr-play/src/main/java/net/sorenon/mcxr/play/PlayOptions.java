a27c596ff6142c58d9b8acc93234dbceeb926518
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-06-18 22:25:55 -0700

    Revert "Add the ability for users to make their own QuickMenu buttons"
    This reverts commit d3786732d3bf7222c4f9fc8782ad4bc50b5ba7f4.

--- mcxr-play/src/main/java/net/sorenon/mcxr/play/PlayOptions.java
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/PlayOptions.java
@@ -1,8 +1,6 @@
 package net.sorenon.mcxr.play;
 
 import com.electronwill.nightconfig.core.file.FileConfig;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
@@ -10,12 +8,9 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import org.lwjgl.glfw.GLFW;
 
-import java.util.ArrayList;
-
 public class PlayOptions {
 
     private static FileConfig fileConfig;
-    private static FileConfig jsonFileConfig;
 
     public static boolean xrUninitialized = false;
     public static boolean xrPaused = false;
@@ -30,9 +25,6 @@ public class PlayOptions {
     public static boolean immersiveControls = true;
     public static boolean teleportEnabled = false;
 
-    public static JsonObject configurations = new JsonObject();
-    public static ArrayList<String> categories = new ArrayList<>();
-
     public static boolean fullMirror = false;
     /**
      * The angle to rotate the player's in-game hand for a more comfortable experience
@@ -46,7 +38,6 @@ public class PlayOptions {
 
     public static void init() {
         fileConfig = FileConfig.of(FabricLoader.getInstance().getConfigDir().resolve("mcxr-play.toml"));
-        jsonFileConfig = FileConfig.of(FabricLoader.getInstance().getConfigDir().resolve("quick-menu.json"));
     }
 
     public static void save() {
@@ -68,7 +59,6 @@ public class PlayOptions {
         fileConfig.set("fullMirror",fullMirror);
 
         fileConfig.set("SSAA", SSAA);
-
         fileConfig.save();
     }
 
@@ -94,17 +84,6 @@ public class PlayOptions {
         immersiveControls = fileConfig.getOrElse("immersiveControls", false);
 
         SSAA = fileConfig.<Number>getOrElse("SSAA", 1).floatValue();
-
-        jsonFileConfig.load();
-
-        int i = 0;
-        JsonArray array = configurations.getAsJsonArray("categories");
-        if(array != null) {
-            while (i > array.size()) {
-                categories.add(array.get(i).getAsString());
-                i++;
-            }
-        }
     }
 
     public enum IndexTouchpad {
--- mcxr-play/src/main/java/net/sorenon/mcxr/play/gui/QuickMenu.java
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/gui/QuickMenu.java
@@ -1,8 +1,5 @@
 package net.sorenon.mcxr.play.gui;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
@@ -10,14 +7,9 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.ChatScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.sorenon.mcxr.play.MCXRPlayClient;
-import net.sorenon.mcxr.play.PlayOptions;
 import net.sorenon.mcxr.play.compat.svc.SimpleVoiceChatCompat;
-import net.sorenon.mcxr.play.mixin.accessor.MouseHandlerAcc;
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.system.CallbackI;
 
 import java.util.ArrayList;
 
@@ -46,16 +38,6 @@ public class QuickMenu extends Screen {
         QuickMenuButtons.add(new Button((this.width/2) - 25, this.height/2, 70, 20, new TranslatableComponent("Chat"), (button ) -> {
             Minecraft.getInstance().setScreen(new XrChatScreen(""));
         }));
-
-
-        for(JsonElement element : PlayOptions.configurations.getAsJsonArray("buttons")) {
-            JsonObject object = element.getAsJsonObject();
-            QuickMenuButtons.add(new Button((width/2) - 25, height/2 + 20, 70 + Minecraft.getInstance().font.width(""), 20, new TextComponent(object.get("name").getAsString()), button -> {
-                MouseHandlerAcc mouseHandler = (MouseHandlerAcc) Minecraft.getInstance().mouseHandler;
-                mouseHandler.callOnPress(Minecraft.getInstance().getWindow().getWindow(),
-                        object.get("key").getAsInt(), GLFW.GLFW_PRESS, 0);
-            }));
-        }
 
         for (int i = 0; i < QuickMenuButtons.size(); i++) {
             Button QuickMenuButton = QuickMenuButtons.get(i);
